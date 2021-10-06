5ec39c9eac24cb2d258d074174b5fb3a0007836c
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2021-10-06 08:13:07 +0200

    Initial work on CardItem component

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
+import CardItem from "../components/CardItem"
 export default function Home() {
   return (
     <div>
       <h1>Home Page</h1>
+      <CardItem/>
     </div>
   )
 }
