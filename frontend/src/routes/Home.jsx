81973442b09e05bbdf7e4cd61c6d31139d692b0c
paccao <16489683+paccao@users.noreply.github.com>
2021-10-07 13:04:43 +0200

    broke out the cardscontainer into a separate component

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,21 +1,9 @@
 import React from "react"
 
-import CardItem from "../components/CardItem/CardItem"
 
-import { Typography, Grid, Container } from "@mui/material"
 
 export default function Home() {
   return (
-    <Container>
-      <Typography variant="h2">Bug tracker</Typography>
-      <Grid container spacing={1.5} marginTop={2}>
-        <CardItem />
-        <CardItem />
-        <CardItem />
-        <CardItem />
-        <CardItem />
-        <CardItem />
-      </Grid>
-    </Container>
+    <h1>Home Page</h1>
   )
 }
