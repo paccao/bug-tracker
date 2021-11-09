faef3e9f83fb9e78657ea1c5fe1920c2b775802e
paccao <16489683+paccao@users.noreply.github.com>
2021-11-09 17:29:12 +0100

    Added Column table in db, some fields are now optionals and others have default values

--- prisma/migrations/20211109150126_initial/migration.sql
+++ prisma/migrations/20211109150126_initial/migration.sql
@@ -1,52 +1,53 @@
--- CreateTable
-CREATE TABLE "User" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "user_name" TEXT NOT NULL,
-    "password" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "avatar" TEXT NOT NULL DEFAULT 'https://loremflickr.com/512/512',
-    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
+/*
+  Warnings:
 
+  - You are about to drop the column `table_id` on the `Card` table. All the data in the column will be lost.
+
+*/
 -- CreateTable
-CREATE TABLE "Board" (
+CREATE TABLE "Column" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "title" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
+    "description" TEXT DEFAULT '',
+    "table_id" INTEGER,
+    "is_archived" BOOLEAN NOT NULL DEFAULT false,
+    CONSTRAINT "Column_table_id_fkey" FOREIGN KEY ("table_id") REFERENCES "Table" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Board" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "description" TEXT DEFAULT '',
     "user_id" INTEGER,
+    "is_archived" BOOLEAN NOT NULL DEFAULT false,
     CONSTRAINT "Board_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User" ("id") ON DELETE SET NULL ON UPDATE CASCADE
 );
-
--- CreateTable
-CREATE TABLE "Table" (
+INSERT INTO "new_Board" ("description", "id", "title", "user_id") SELECT "description", "id", "title", "user_id" FROM "Board";
+DROP TABLE "Board";
+ALTER TABLE "new_Board" RENAME TO "Board";
+CREATE TABLE "new_Table" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "title" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
+    "description" TEXT DEFAULT '',
     "board_id" INTEGER,
+    "is_archived" BOOLEAN NOT NULL DEFAULT false,
     CONSTRAINT "Table_board_id_fkey" FOREIGN KEY ("board_id") REFERENCES "Board" ("id") ON DELETE SET NULL ON UPDATE CASCADE
 );
-
--- CreateTable
-CREATE TABLE "Card" (
+INSERT INTO "new_Table" ("board_id", "description", "id", "title") SELECT "board_id", "description", "id", "title" FROM "Table";
+DROP TABLE "Table";
+ALTER TABLE "new_Table" RENAME TO "Table";
+CREATE TABLE "new_Card" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "title" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "table_id" INTEGER,
-    CONSTRAINT "Card_table_id_fkey" FOREIGN KEY ("table_id") REFERENCES "Table" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+    "description" TEXT DEFAULT '',
+    "column_id" INTEGER,
+    "is_archived" BOOLEAN NOT NULL DEFAULT false,
+    CONSTRAINT "Card_column_id_fkey" FOREIGN KEY ("column_id") REFERENCES "Column" ("id") ON DELETE SET NULL ON UPDATE CASCADE
 );
-
--- CreateTable
-CREATE TABLE "Assigned_users" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "card_id" INTEGER,
-    "user_id" INTEGER,
-    CONSTRAINT "Assigned_users_card_id_fkey" FOREIGN KEY ("card_id") REFERENCES "Card" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
-    CONSTRAINT "Assigned_users_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User" ("id") ON DELETE SET NULL ON UPDATE CASCADE
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_user_name_key" ON "User"("user_name");
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+INSERT INTO "new_Card" ("description", "id", "title") SELECT "description", "id", "title" FROM "Card";
+DROP TABLE "Card";
+ALTER TABLE "new_Card" RENAME TO "Card";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
