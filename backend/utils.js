315e763a7b7572b60a7667da9918e09c9205286d
paccao <16489683+paccao@users.noreply.github.com>
2021-11-09 10:18:25 +0100

    nextjs initial setup + custom favicon

--- backend/utils.js
+++
@@ -1,7 +0,0 @@
-
-/** hash password using bcrypt 
- * ! Right now bcrypt is vulnerable (October 4th, 2021)
- */
-export function hashPass(rawPass) {
-  
-}
--- frontend/src/components/CardItem/cardItems.js
+++
@@ -1,3 +0,0 @@
-export function functionality(){
-  return "Card Item Text"
-}
