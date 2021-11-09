6f01079cbc983f8e1da9bdea7d948795686ea6fe
paccao <16489683+paccao@users.noreply.github.com>
2021-11-09 10:19:55 +0100

    dev branch created

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
 			<main className={styles.main}>
 				<h1 className={styles.title}>
-					Welcome to <a href="https://nextjs.org">Next.js!</a>
+					Welcome to <a href="https://github.com/paccao/bug-tracker">Bug tracker!</a>
 				</h1>
 
 				<p className={styles.description}>
