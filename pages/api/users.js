97aa150b11d6ced586af49e904676a3bc6ca098c
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2021-11-12 17:28:48 +0100

    users read finished, create has bugs

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,6 +1,29 @@
 import prisma from "../../lib/prisma"
 
-export default function handler(req, res) {
-	console.log(prisma)
-	res.status(200).json({ name: "John Doe" })
+export default async function handler(req, res) {
+	try {
+		console.log(req.method)
+		switch (req.method) {
+			case "GET":
+				const users = await prisma.user.findMany()
+				res.status(200).json({ success: true, users })
+				break
+			case "POST":
+				const createdUser = await prisma.user.create(req.body)
+				res.status(200).json({
+					success: true,
+					message: "User created.",
+					createdUser,
+				})
+				break
+			default:
+				res.status(200).json({
+					success: false,
+					message: "Api method not set up yet.",
+				})
+				break
+		}
+	} catch (error) {
+		res.status(400).json({ message: error.message })
+	}
 }
